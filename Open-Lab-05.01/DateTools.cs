a1197d7569b273324c953e4805dc57c73002c863
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-23 21:15:01 +0100

    hotovo

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,7 +6,16 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-            throw new NotImplementedException();
+            if (year <= 0)
+                return "0 and negative is not allow for a year";
+             
+            else if (year <= 100)
+                return "1st century";
+
+            else if (year % 100 == 0)
+                return year / 100 + "th century";
+            else
+                return year / 100 + 1 + "th century";
         }
     }
 }
